775d264218fc7bd46b9c5d2c69ad5bec7722d5af
chebon254 <kelvinchebon90@gmail.com>
2024-10-30 13:56:13 +0300

    Interfaces

--- public/app.js
+++ public/app.js
@@ -1,11 +1,20 @@
-"use strict";
 // const anchor = document.querySelector('a')!;
 // console.log(anchor.href);
 // const form = document.querySelector('form')!; // Picks tags
-const form = document.querySelector('.new-item-form'); // Picks tag using  classes(Casting)
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(type.value, tofrom.valueAsNumber);
-});
+// const form = document.querySelector('.new-item-form') as HTMLFormElement; // Picks tag using  classes(Casting)
+// const type = document.querySelector('#type') as HTMLFormElement;
+// const tofrom = document.querySelector('#tofrom') as HTMLFormElement;
+// form.addEventListener('submit', (e: Event) => {
+//     e.preventDefault();
+//     console.log(
+//         type.value,
+//         tofrom.valueAsNumber
+//     );
+// })
+import { Invoice } from './classes/Invoice.js';
+const invoiceOne = new Invoice('Mario', 'work on website', 250);
+const invoiceTwo = new Invoice('Chebon', 'work on website', 50);
+console.log(invoiceOne, invoiceTwo);
+let invoices = [];
+// Access Modifiers: private, public, readonly
+// Interfaces
--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,48 @@
 // console.log(anchor.href);
 
 // const form = document.querySelector('form')!; // Picks tags
-const form = document.querySelector('.new-item-form') as HTMLFormElement; // Picks tag using  classes(Casting)
+// const form = document.querySelector('.new-item-form') as HTMLFormElement; // Picks tag using  classes(Casting)
 
-const type = document.querySelector('#type') as HTMLFormElement;
-const tofrom = document.querySelector('#tofrom') as HTMLFormElement;
+// const type = document.querySelector('#type') as HTMLFormElement;
+// const tofrom = document.querySelector('#tofrom') as HTMLFormElement;
 
-form.addEventListener('submit', (e: Event) => {
-    e.preventDefault();
+// form.addEventListener('submit', (e: Event) => {
+//     e.preventDefault();
 
-    console.log(
-        type.value,
-        tofrom.valueAsNumber
-    );
+//     console.log(
+//         type.value,
+//         tofrom.valueAsNumber
+//     );
     
-})
+// })
+
+
+
+
+
+
+
+
+import { Invoice } from './classes/Invoice.js'
+
+
+
+
+const invoiceOne = new Invoice('Mario', 'work on website', 250);
+const invoiceTwo = new Invoice('Chebon', 'work on website', 50);
+
+console.log(invoiceOne, invoiceTwo);
+
+let invoices: Invoice[] = []
+
+// Access Modifiers: private, public, readonly
+
+
+
+
+// Interfaces
+
+
+
+
+
